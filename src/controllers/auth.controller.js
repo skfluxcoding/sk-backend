660cc4a256899ccf6bf8f3302537471bc39c3bd3
SK Flux Coding <skfluxcoding@gmail.com>
2026-02-04 21:54:41 -0500

    Set method login, delete refresh, audti functions, remove models

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,9 +1,6 @@
 const User = require('../models/user.model');
-const RefreshToken = require('../models/refresh-token.model');
 const passwordUtil = require('../utils/password.util');
 const jwtUtil = require('../utils/jwt.util');
-const refreshUtil = require('../utils/refresh-token.util');
-const audit = require('../utils/audit.util');
 
 exports.register = async (req, res) => {
   const { email, password } = req.body;
@@ -23,85 +20,15 @@ exports.login = async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
-  console.log(user);
   
   if (!user) return res.sendStatus(401);
 
   const match = await passwordUtil.compare(password, user.password);
-  if (!match) return res.sendStatus(401);
-
-  /*await audit.log({
-    userId: user._id,
-    event: 'LOGIN',
-    req
-  });*/
+  if (!match) {
+    return res.status(400).json({ message: 'Invalid credentials' });
+  }
 
   const token = jwtUtil.generateAccessToken(user);
 
-  const refreshToken = jwtUtil.generateRefreshToken(user);
-
-  await RefreshToken.create({
-    userId: user._id,
-    token: refreshToken,
-    expiresAt: refreshUtil.expirationDate()
-  });
-
-  res.json({ accessToken: token, refreshToken });
+  res.json({ accessToken: token });
 };
-
-exports.refresh = async (req, res) => {
-  const { refreshToken } = req.body;
-  if (!refreshToken) return res.sendStatus(400);
-
-  const stored = await RefreshToken.findOne({ token: refreshToken });
-
-  // token inexistente o ya revocado = ataque
-  if (!stored || stored.revoked) {
-    await RefreshToken.updateMany(
-      { userId: stored.userId },
-      { revoked: true }
-    );
-    return res.sendStatus(401);
-  }
-
-  if (stored.expiresAt < new Date()) {
-    return res.sendStatus(401);
-  }
-
-  const user = await User.findById(stored.userId);
-  if (!user) return res.sendStatus(401);
-
-  const newRefreshToken = refreshUtil.generate();
-
-  stored.revoked = true;
-  stored.replacedByToken = newRefreshToken;
-  await stored.save();
-
-  await RefreshToken.create({
-    userId: user._id,
-    token: newRefreshToken,
-    expiresAt: refreshUtil.expirationDate()
-  });
-
-  const newAccessToken = jwtUtil.sign(user);
-
-  res.json({
-    accessToken: newAccessToken,
-    refreshToken: newRefreshToken
-  });
-};
-
-
-exports.logout = async (req, res) => {
-  const { refreshToken } = req.body;
-
-  if (!refreshToken) return res.sendStatus(400);
-
-  await RefreshToken.updateOne(
-    { token: refreshToken },
-    { revoked: true }
-  );
-
-  res.sendStatus(204);
-};
-
