5f522c2b6456915525c5a907ee5fb1ca95568ebb
SK Flux Coding <skfluxcoding@gmail.com>
2026-02-07 23:38:48 -0500

    feat(student): add routes with ObjectId middleware

---
+++ src/routes/student.routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const controller = require('../controllers/student.controller');
+const validateObjectId = require('../middleware/validateObjectId.middleware');
+
+router.post('/', controller.create);
+router.get('/', controller.list);
+router.get('/:id', validateObjectId, controller.get);
+router.put('/:id', validateObjectId, controller.update);
+router.delete('/:id', validateObjectId, controller.remove);
+
+module.exports = router;
