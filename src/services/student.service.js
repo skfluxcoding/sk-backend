df9415e8461cae2f73f1637405b99e90bd6a99fe
SK Flux Coding <skfluxcoding@gmail.com>
2026-02-08 17:50:01 -0500

    feat(student): implement soft delete in service layer

--- src/services/student.service.js
+++ src/services/student.service.js
@@ -4,5 +4,6 @@ exports.create = (data) => Student.create(data);
 exports.findAll = () => Student.find({ deleted: false });
 exports.findById = (id) => Student.findOne({ _id: id, deleted: false });
 exports.update = (id, data) =>
-  Student.findByIdAndUpdate(id, data, { new: true });
-exports.remove = (id) => Student.findByIdAndDelete(id);
+  Student.findOneAndUpdate({ _id: id, deleted: false }, data, { new: true });
+exports.remove = (id) =>
+  Student.findByIdAndUpdate(id, { deleted: true }, { new: true });
