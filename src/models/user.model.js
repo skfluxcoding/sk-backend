8490096bf8f6caec4f6339ba541bee8dda3960e5
Siulppe King <elchancateclas@gmail.com>
2026-02-19 11:03:29 -0500

    Set User m

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -16,4 +16,5 @@ const UserSchema = new mongoose.Schema({
   }
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
