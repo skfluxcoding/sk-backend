a30aced32d6aa3ffbf6288230c7c7a5be5ee4b21
SK Flux Coding <skfluxcoding@gmail.com>
2026-02-19 19:24:23 -0500

    User Controller Modified for Create User

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,11 +3,6 @@ const userService = require('../services/user.service');
 
 exports.create = async (req, res) => {
     const user = await userService.create(req.body);
-
-    const code = Math.floor(100000 + Math.random() * 900000);
-
-    const info = await sendVerificationCode(user.email, code);
-    console.log(info);
-    
     return res.status(201).json(user);
 }
+
